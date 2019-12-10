7260e790985396a6062524f0b09bdc1339ffbb53
SmischenkoB <bogdan.smishenko@gmail.com>
2019-12-10 12:04:54 +0200

    Rebuild
    Replacing

---
+++ EurekaClient1/src/main/java/some/GreetingController.java
@@ -0,0 +1,12 @@
+package some;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    @RequestMapping("/greeting")
+    String greeting() {
+        return ".";
+    }
+}
